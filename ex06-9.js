37213670d60df98896efa5fb7ab0b1bebcc6b127
zm17346 <zm17346@josai.ac.jp>
2019-02-03 22:13:53 -0500

    hello

---
+++ ex06-9.js
@@ -0,0 +1,23 @@
+function txtchange1() {
+  document.getElementById('txt1').innerHTML = 'ナンバーワン！';
+}
+
+function imgchange5() {
+  document.getElementById('logo').src = './image/bulbon.jpg';
+}
+
+function imgchange1() {
+  document.getElementById('hipumai').src = './image/ヒプノシスマイク_BB.png';
+}
+function imgchange2() {
+  document.getElementById('hipumai').src = './image/ヒプノシスマイク_MTC.jpg';
+}
+function imgchange3() {
+  document.getElementById('hipumai').src = './image/ヒプノシスマイク_FP.jpg';
+}
+function imgchange4() {
+  document.getElementById('hipumai').src = './image/ヒプノシスマイク_摩天狼.jpg';
+}
+
+
+
